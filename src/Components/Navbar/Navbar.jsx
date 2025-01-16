9d1010f21f5ba7ba3204c60523beaf566a4288db
Norhan Hamed <62403829+norhanhamed@users.noreply.github.com>
2025-01-17 00:13:31 +0200

    auth

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,103 +16,98 @@ export default function Navbar() {
           <h1 className="logo">
             <img src={logo} alt="" />
           </h1>
-          {token ? (
-            <>
-              {/* navigation ul */}
-              <ul className='flex gap-6 items-center'>
-                <li>
-                  <NavLink to=""
-                    className={({ isActive }) => {
-                      return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
+          {token ? {/* navigation ul */ }
+            (<ul className='flex gap-6 items-center'>
+              <li>
+                <NavLink to=""
+                  className={({ isActive }) => {
+                    return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
                  before:duration-300 before:bg-primary before:absolute before:left-0 before:-bottom-1
                
                 ${isActive ? "font-bold before:w-full" : "before:w-0"}`
-                    }}
-                  >Home </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/allProducts"
-                    className={({ isActive }) => {
-                      return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
+                  }}
+                >Home </NavLink>
+              </li>
+              <li>
+                <NavLink to="/allProducts"
+                  className={({ isActive }) => {
+                    return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
                  before:duration-300 before:bg-primary before:absolute before:left-0 before:-bottom-1
                
                 ${isActive ? "font-bold before:w-full" : "before:w-0"}`
-                    }}>Products </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/categories/id"
-                    className={({ isActive }) => {
-                      return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
+                  }}>Products </NavLink>
+              </li>
+              <li>
+                <NavLink to="/categories/id"
+                  className={({ isActive }) => {
+                    return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
                  before:duration-300 before:bg-primary before:absolute before:left-0 before:-bottom-1
                 ${isActive ? "font-bold before:w-full" : "before:w-0"}`
-                    }}>
-                    Categories </NavLink>
-                </li>
-                <li>
-                  <NavLink to="brands"
-                    className={({ isActive }) => {
-                      return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
+                  }}>
+                  Categories </NavLink>
+              </li>
+              <li>
+                <NavLink to="brands"
+                  className={({ isActive }) => {
+                    return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
                  before:duration-300 before:bg-primary before:absolute before:left-0 before:-bottom-1
                 ${isActive ? "font-bold before:w-full" : "before:w-0"}`
-                    }}>Brands </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/allorders"
-                    className={({ isActive }) => {
-                      return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
+                  }}>Brands </NavLink>
+              </li>
+              <li>
+                <NavLink to="/allorders"
+                  className={({ isActive }) => {
+                    return `relative before:h-[2px]  hover:before:w-full hover:font-bold before:transition-[width]
                  before:duration-300 before:bg-primary before:absolute before:left-0 before:-bottom-1
                 ${isActive ? "font-bold before:w-full" : "before:w-0"}`
-                    }}>Orders </NavLink>
-                </li>
-
-              </ul>
-              
-              {/* wishIcon + cartIcon */}
-              <ul className='flex gap-6 items-center ms-auto '>
-                <li>
-                  <Link to="/cart"
-                    className="ms-auto relative">
-                    <i className='fa-solid fa-cart-shopping text-lg'></i>
-                    <span className='bg-primary absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full text-sm font-bold text-white flex justify-center items-center'>
-                      {cartInfo === null ? <i className='fa-solid fa-spinner fa-spin '></i> : cartInfo.numOfCartItems || 0}
-                    </span>
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/wishList"
-                    className={`hover:transition-all hover:text-red-600 hover:duration-300 hover:animate-bounce `}>
-                    <i className="fa-regular fa-heart text-xl font-bold "></i>
-                  </Link>
-                </li>
-
-
-              </ul>
-              {/* media ul  */}
-              <ul className='flex gap-6 items-center  '>
-                <li>
-                  <Link to="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-link">
-                    <i className="fa-brands fa-facebook"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-link">
-                    <i className="fa-brands fa-twitter"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-link">
-                    <i className="fa-brands fa-instagram"></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="social-link">
-                    <i className="fa-brands fa-linkedin"></i>              </Link>
-                </li>
-              </ul>
-            </>)
+                  }}>Orders </NavLink>
+              </li>
+            </ul>)
             : ("")
           }
+
+          {/* wishIcon + cartIcon */}
+          <ul className='flex gap-6 items-center ms-auto '>
+            {token ?
+              (
+                  <li>
+                    <Link to="/cart"
+                      className="ms-auto relative">
+                      <i className='fa-solid fa-cart-shopping text-lg'></i>
+                      <span className='bg-primary absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 w-6 h-6 rounded-full text-sm font-bold text-white flex justify-center items-center'>
+                        {cartInfo === null ? <i className='fa-solid fa-spinner fa-spin '></i> : cartInfo.numOfCartItems || 0}
+                      </span>
+                    </Link>
+                  </li>)
+              : ("")}
+          </ul>
+          {/* media ul  */}
+          {token? (
+            <ul className='flex gap-6 items-center  '>
+            <li>
+              <Link to="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-link">
+                <i className="fa-brands fa-facebook"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-link">
+                <i className="fa-brands fa-twitter"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-link">
+                <i className="fa-brands fa-instagram"></i>
+              </Link>
+            </li>
+            <li>
+              <Link to="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="social-link">
+                <i className="fa-brands fa-linkedin"></i>              </Link>
+            </li>
+          </ul>
+
+          ):("")}
+          
+
 
           {/* authentication ul */}
           <ul className="flex gap-6 items-center ms-auto" >
